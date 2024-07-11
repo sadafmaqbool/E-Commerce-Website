a820591ab53aaaded0cabb80fd6356557e76f645
sadafmaqbool <sadafmaqbool71@gmail.com>
2024-07-11 21:43:26 +0500

    update code

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,20 +10,6 @@ import { useAppSelector } from "@/lib/supabase/hooks/redux";
 import { getCart } from "@/redux/cartSlice";
 import { supabase } from "@/lib/supabase/products";
 
-const itemList = [
-  "All",
-  "Fresh",
-  "Amazon miniTV",
-  "Sell",
-  "Gift Cards",
-  "Baby",
-  "Buy Again",
-  "Browsing History",
-  "Amazon Pay",
-  "Gift Ideas",
-  "Health, Household & Personal Care",
-];
-
 const Header = () => {
   const [query, setQuery] = useState<string>("");
   const [user, setUser] = useState<any>(null);
@@ -48,11 +34,11 @@ const Header = () => {
   return (
     <>
       <div className="bg-[#131921] text-white py-1">
-        <div className="flex items-center justify-between w-[90%] mx-auto">
+        <div className="flex flex-wrap items-center justify-between w-[90%] mx-auto">
           <Link href={"/"} className="w-[10%]">
             <Image src={amazonLogo} alt={"logo"} width={150} height={150} />
           </Link>
-          <div className="flex items-center w-[60%]">
+          <div className="flex items-center w-[50%] sm:w-[58%]">
             <input
               value={query}
               onChange={(e) => setQuery(e.target.value)}
@@ -62,29 +48,31 @@ const Header = () => {
             />
             <div
               onClick={searchHandler}
-              className="bg-[#FEBD69] p-2 cursor-pointer hover:bg-[#ffad43]   rounded-r-md"
+              className="bg-[#FEBD69] p-2 cursor-pointer hover:bg-[#ffad43] rounded-r-md"
             >
               <CgSearch size={"24px"} className="text-black" />
             </div>
           </div>
-          <div className="flex items-center justify-around w-[20%]">
+          <div className="flex items-center justify-around w-[40%] sm:w-[30%] mt-2 sm:mt-0">
             <div
               onClick={() => {
                 router.push("/signin");
               }}
               className="cursor-pointer"
             >
-              <h1 className="text-xs hover:underline">{`${
-                user ? user?.identities[0]?.identity_data.full_name : "Signin"
-              }`}</h1>
+              <h1 className="text-xs hover:underline">
+                {user ? user?.identities[0]?.identity_data.full_name : "Signin"}
+              </h1>
               <h1 className="font-medium text-sm">Account & Lists</h1>
             </div>
             <div>
               <p className="text-xs">Returns</p>
               <h1 className="font-medium text-sm">& Orders</h1>
             </div>
-            <Link href={"/cart"} className="cursor-pointer">
-              <p className="relative top-3 left-5">{cart.length}</p>
+            <Link href={"/cart"} className="cursor-pointer relative">
+              <p className="absolute top-0 left-6 bg-red-600 text-white rounded-full px-2 text-xs">
+                {cart.length}
+              </p>
               <div className="flex">
                 <div>
                   <BiCart size={"40px"} />
@@ -92,33 +80,18 @@ const Header = () => {
                 <h1 className="mt-4">cart</h1>
               </div>
             </Link>
-          </div>
-        </div>
-      </div>
-      <div className="bg-[#232F3E] w-full text-white p-2  flex justify-between items-center">
-        <div>
-          {itemList.map((link, idx) => {
-            return (
-              <Link
-                key={idx}
-                href={`/${link}`}
-                className="mx-2 hover:border border border-transparent hover:border-white p-2"
+            <div className="cursor-pointer">
+              <h1
+                onClick={async () => {
+                  const { error } = await supabase.auth.signOut();
+                  router.push("/signin");
+                }}
+                className="text-[#FEBD69] font-bold hover:underline"
               >
-                {link}
-              </Link>
-            );
-          })}
-        </div>
-        <div className="mr-5">
-          <h1
-            onClick={async () => {
-              const { error } = await supabase.auth.signOut();
-              router.push("/signin");
-            }}
-            className="text-[#FEBD69] font-bold cursor-pointer hover:underline"
-          >
-            Sign out
-          </h1>
+                Sign out
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </>
